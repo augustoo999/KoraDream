b5dacabf31da98e2fedc0645f24dca805d37971c
Pedro Augusto <pbernardoguilherme@gmail.com>
2024-04-22 18:36:20 -0300

    Update Enemy2.cs

--- JogoDePlataformaSimples/Assets/Scripts/Enemy/Enemy2.cs
+++ JogoDePlataformaSimples/Assets/Scripts/Enemy/Enemy2.cs
@@ -6,12 +6,12 @@ public class Enemy2 : MonoBehaviour
 {
     public float speed;
     public float distance;
-    private bool isRight = true;
     public Transform groundCheck;
     public LayerMask layer;
     [SerializeField]
     private GameObject GameOverMenu;
     public Rigidbody2D rb;
+    private bool isRight;
 
     private void Start()
     {
@@ -30,20 +30,30 @@ public class Enemy2 : MonoBehaviour
 
     void Check()
     {
+        // Lança um raio para a direita
         RaycastHit2D direita = Physics2D.Raycast(transform.position, Vector2.right, distance, layer);
+        Debug.Log("Raycast Direita Detectado");
+        // Lança um raio para a esquerda
         RaycastHit2D esquerda = Physics2D.Raycast(transform.position, Vector2.left, distance, layer);
+        Debug.Log("Raycast Esquerdo Detectado");
+
+        // Verifica se houve colisão com a direita
         if (direita.collider == true)
-        { 
+        {
+            Debug.Log("Collider à direita");
             isRight = true;
-            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
-        
+
+        // Verifica se houve colisão com a esquerda
         else if (esquerda.collider == true)
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
+            Debug.Log("Collider à esquerda");
+            transform.eulerAngles = new Vector3(0, 180, 0);
             isRight = false;
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player") && rb.velocity.y <= 0)
